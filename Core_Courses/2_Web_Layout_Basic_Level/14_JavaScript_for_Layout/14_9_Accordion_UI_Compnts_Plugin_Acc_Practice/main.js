9e438f0167396d745670799b3290de250468943d
SergeyS2K <79189126+SergeyS2K@users.noreply.github.com>
2023-05-16 19:11:34 +0300

    started/ended topic: 14_9_Accordion_UI_Compnts_Plugin_Acc_Practice / deserve(s) attention: work with plugins, UI (folder/files creation)

---
+++ Core_Courses/2_Web_Layout_Basic_Level/14_JavaScript_for_Layout/14_9_Accordion_UI_Compnts_Plugin_Acc_Practice/main.js
@@ -0,0 +1,6 @@
+new Accordion('.accordion-list', {
+  elementClass: 'list-item',
+  triggerClass: 'accordion-list__control',
+  panelClass: 'accordion-list__content',
+  activeClass: 'accordion_active',
+});
