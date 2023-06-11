0838792f295de25be4453025ba8b2e007fea5f5c
SergeyS2K <79189126+SergeyS2K@users.noreply.github.com>
2023-06-11 15:27:09 +0300

    practice/started: 17_5_Practice_Bootstrap_Lioniс (folder/files creation/copying)

---
+++ Core_Courses/2_Web_Layout_Basic_Level/17_Grids/17_5_Practice_Bootstrap_Lioniс/main.js
@@ -0,0 +1,18 @@
+// burger menu / mobile
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.nav__link');
+
+burger.addEventListener('click', function () {
+  burger.classList.toggle('burger_active');
+  menu.classList.toggle('header__nav_active');
+  document.body.classList.toggle('stop-scroll');
+});
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function () {
+    burger.classList.remove('burger_active');
+    menu.classList.remove('header__nav_active');
+    document.body.classList.remove('stop-scroll');
+  });
+});
---
+++ Core_Courses/2_Web_Layout_Basic_Level/17_Grids/17_5_Practice_Bootstrap_Lioniс/notes.md
@@ -0,0 +1,9 @@
+# On the topic:
+
+## Video-lecture:
+
+[click..](https://go.skillbox.ru/profession/profession-fullstack-js/weblayout/f7bdbe5e-95b9-4d22-ae3d-5a4c2804f5c0/videolesson)
+
+## A few words:
+
+container.. row.. col..
