dc8f01cd0cabf94a4cb5ca4b6682b36e8db58084
S2kDev <79189126+S2kDev@users.noreply.github.com>
2023-09-09 23:36:23 +0300

    [LES-2.2/st-compl] Add f/f 2_2_Numbers_num_systems
    Working with numbers, defining number systems.
    
    Worth noting:
      - Importance of understanding this topic.
    
    Core: B-3 / JS-BL

---
+++ Core_Courses/3_JS_Basic_Level/2_Variables_Numbers/2_2_Numbers_num_systems/index.js
@@ -0,0 +1,21 @@
+let digit = 25;
+let fract = 25.03;
+let exp = 2e10; // 20,000,000,000 (20 billion)
+let expFract = 1.25e3; // 1250
+
+// Binary / bin number system (translation of decimal / dec system (10))
+
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+// 0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001
+
+let dec = 9;
+let bin = 0b1001; // => 9
+
+// Octal / oct number system (8)
+
+let oct = 0o10; // => 8
+let oct2 = 0o32; // => 26
+
+// Hexadecimal / hex number system (16)
+
+let hex = 0x1a; // => 26
---
+++ Core_Courses/3_JS_Basic_Level/2_Variables_Numbers/2_2_Numbers_num_systems/notes.md
@@ -0,0 +1,9 @@
+# On the topic:
+
+### Video-lecture:
+
+[2.2. Numbers. Number systems](https://go.skillbox.ru/profession/profession-fullstack-js/js/9a10afc3-b6cf-4891-8c5a-8c7084aec0e7/videolesson)
+
+### A few words:
+
+Number systems.. fundamental stuff!
