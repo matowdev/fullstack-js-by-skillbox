7ebc994e255a9ad21d5918c7dd8529e12b961167
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-09-26 17:07:12 +0300

    [PW-2.8/cont] Add second task
    Rounding/floor and comparing fractional parts of numbers.
    
    Worth noting:
      - the remainder of division by 1 returns the fractional part.
    
    Core: B-3 / JS-BL

--- Core_Courses/3_JS_Basic_Level/2_Variables_Numbers/2-8-pw-2-variables-numbers/index.js
+++ Core_Courses/3_JS_Basic_Level/2_Variables_Numbers/2-8-pw-2-variables-numbers/index.js
@@ -11,4 +11,26 @@ getRectangularArea(5, 8, 5, 5); // 0
 getRectangularArea(8, 1, 5, 1); // 0
 
 // вторая задача
-function getFractionalPart(a, b, n) {}
+function getFractionalPart(a, b, precision) {
+  let partA = Math.floor((a % 1) * Math.pow(10, precision));
+  let partB = Math.floor((b % 1) * Math.pow(10, precision));
+  console.log('Дробная часть, числа A: ' + partA);
+  console.log('Дробная часть, числа B: ' + partB);
+  console.log('Их сравнение:');
+  comparison(partA, partB);
+}
+
+function comparison(a, b) {
+  console.log('часть A > части B:', a > b);
+  console.log('часть A < части B:', a < b);
+  console.log('часть A >= части B:', a >= b);
+  console.log('часть A <= части B:', a <= b);
+  console.log('часть A === части B:', a === b);
+  console.log('часть A !== части B:', a !== b);
+}
+
+getFractionalPart(13.123456789, 2.123, 5);
+console.log('-----');
+getFractionalPart(13.890123, 2.891564, 2);
+console.log('-----');
+getFractionalPart(13.890123, 2.891564, 3);
