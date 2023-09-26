8f30da8e8ed0473e45fe8bcb792793278648256d
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-09-26 18:02:29 +0300

    [PW-2.8/cont] Correct output, return
    Making some external changes, adding return.
    
    Core: B-3 / JS-BL

--- Core_Courses/3_JS_Basic_Level/2_Variables_Numbers/2-8-pw-2-variables-numbers/index.js
+++ Core_Courses/3_JS_Basic_Level/2_Variables_Numbers/2-8-pw-2-variables-numbers/index.js
@@ -1,14 +1,15 @@
 // первая задача
 function getRectangularArea(x1, y1, x2, y2) {
   let rectangularArea = Math.abs(x1 - x2) * Math.abs(y1 - y2);
-  return console.log(rectangularArea);
+  return console.log('Площадь составляет:', rectangularArea);
 }
 
-getRectangularArea(2, 3, 10, 5); // 16
-getRectangularArea(10, 5, 2, 3); // 16
-getRectangularArea(-5, 8, 10, 5); // 45
-getRectangularArea(5, 8, 5, 5); // 0
-getRectangularArea(8, 1, 5, 1); // 0
+getRectangularArea(2, 3, 10, 5);
+getRectangularArea(10, 5, 2, 3);
+getRectangularArea(-5, 8, 10, 5);
+getRectangularArea(5, 8, 5, 5);
+getRectangularArea(8, 1, 5, 1);
+console.log('');
 
 // вторая задача
 function getFractionalPart(a, b, precision) {
@@ -17,7 +18,7 @@ function getFractionalPart(a, b, precision) {
   console.log('Дробная часть, числа A: ' + partA);
   console.log('Дробная часть, числа B: ' + partB);
   console.log('Их сравнение:');
-  comparison(partA, partB);
+  return comparison(partA, partB);
 }
 
 function comparison(a, b) {
@@ -27,10 +28,9 @@ function comparison(a, b) {
   console.log('часть A <= части B:', a <= b);
   console.log('часть A === части B:', a === b);
   console.log('часть A !== части B:', a !== b);
+  return console.log('');
 }
 
 getFractionalPart(13.123456789, 2.123, 5);
-console.log('-----');
 getFractionalPart(13.890123, 2.891564, 2);
-console.log('-----');
 getFractionalPart(13.890123, 2.891564, 3);
