a47b841bfd48e48f50f683839c253d1782160da0
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-09-27 19:27:57 +0300

    [PW-2.8/compl] Add third task
    Gen. two random num's between n and m inclusive, followed by comparison.
    
    Worth noting:
      - getting rid of the loss of accuracy.
    
    Core: B-3 / JS-BL

--- Core_Courses/3_JS_Basic_Level/2_Variables_Numbers/2-8-pw-2-variables-numbers/index.js
+++ Core_Courses/3_JS_Basic_Level/2_Variables_Numbers/2-8-pw-2-variables-numbers/index.js
@@ -1,4 +1,4 @@
-// первая задача
+// задача #1
 function getRectangularArea(x1, y1, x2, y2) {
   let rectangularArea = Math.abs(x1 - x2) * Math.abs(y1 - y2);
   return console.log('Площадь составляет:', rectangularArea);
@@ -11,7 +11,7 @@ getRectangularArea(5, 8, 5, 5);
 getRectangularArea(8, 1, 5, 1);
 console.log('');
 
-// вторая задача
+// задача #2
 function getFractionalPart(a, b, precision) {
   let partA = Math.floor((a % 1) * Math.pow(10, precision));
   let partB = Math.floor((b % 1) * Math.pow(10, precision));
@@ -34,3 +34,32 @@ function comparison(a, b) {
 getFractionalPart(13.123456789, 2.123, 5);
 getFractionalPart(13.890123, 2.891564, 2);
 getFractionalPart(13.890123, 2.891564, 3);
+
+// задача #3
+function getRandomNumbers(n, m) {
+  let min = Math.min(n, m);
+  let num1 = n + Math.random() * (m + 1 - n);
+  let num2 = n + Math.random() * (m + 1 - n);
+  console.log(
+    'Таким образом получаем:',
+    Math.floor((min + num1) * 10) / 10 +
+      ' и ' +
+      Math.floor((min + num2) * 10) / 10
+  );
+  return numComparison(num1, num2);
+}
+
+function numComparison(n, m) {
+  console.log('число 1 > числа 2:', n > m);
+  console.log('число 1 < числа 2:', n < m);
+  console.log('число 1 >= числа(у) 2:', n >= m);
+  console.log('число 1 <= числа(у) 2:', n <= m);
+  console.log('число 1 === числу 2:', n === m);
+  console.log('число 1 !== числу 2:', n !== m);
+  return console.log('');
+}
+
+getRandomNumbers(0, 100);
+getRandomNumbers(2, 5);
+getRandomNumbers(100, -5);
+getRandomNumbers(-3, -10);
