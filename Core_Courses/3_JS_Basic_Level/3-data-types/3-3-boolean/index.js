d791b707dedf80840bfa269f899b257a5705e51f
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-10-07 07:46:20 +0300

    [LES-3.3/st-compl] Boolean
    Some interaction with Boolean entry, values.
    
    Core: B-3 / JS-BL

---
+++ Core_Courses/3_JS_Basic_Level/3-data-types/3-3-boolean/index.js
@@ -0,0 +1,21 @@
+let yep = true;
+let nope = false;
+
+let yes = 5 > 3;
+let no = 5 < 3;
+console.log('5 > 3 ' + yes + ', а ' + '5 < 3 ' + no); // 5 > 3 true, а 5 < 3 false
+
+// инвертирование
+let alsoNo = !yes;
+console.log(alsoNo); // false
+
+// условия
+let x = 10;
+let y = 20;
+
+if (x > y) {
+  console.log('x больше y');
+}
+if (x < y) {
+  console.log('x меньше y'); // x меньше y
+}
---
+++ Core_Courses/3_JS_Basic_Level/3-data-types/3-3-boolean/notes.md
@@ -0,0 +1,5 @@
+# On the topic:
+
+### Video-lecture:
+
+[3.3. Boolean](https://go.skillbox.ru/profession/paket-autotesting-javascript/js/7074e7c9-091c-4ff4-a017-a43b8bc0dc60/videolesson)
