ac8777c7a05aa5c602a553b31d15f96b44e13fec
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-10-10 14:18:58 +0300

    [PW-3.9/st] Add first task
    Password verification, interact. with if/else and logical operators.
    
    Core: B-3 / JS-BL

---
+++ Core_Courses/3_JS_Basic_Level/3-data-types/3-9-pw-3-data-types/notes.md
@@ -0,0 +1,5 @@
+# On the topic:
+
+### Terms of performance:
+
+[3.9. PW #3. Data types](https://go.skillbox.ru/profession/profession-fullstack-js/js/3665cca1-96fe-4cfd-8f12-b996a989f03e/homework)
---
+++ Core_Courses/3_JS_Basic_Level/3-data-types/3-9-pw-3-data-types/task1.js
@@ -0,0 +1,20 @@
+function checkPassword(password) {
+  password = password.toString();
+  if (
+    password.length >= 4 &&
+    (password.includes('-') || password.includes('_'))
+  ) {
+    return console.log('Ого, надёжный пароль! Вы ввели: ' + password);
+  } else {
+    return console.log('Внимание! Ненадёжный пароль.. Вы ввели: ' + password);
+  }
+}
+
+checkPassword(123456789); // Внимание! Ненадёжный пароль.. Вы ввели: 123456789
+checkPassword('1234-'); // Ого, надёжный пароль! Вы ввели: 1234-
+checkPassword('4321_'); // Ого, надёжный пароль! Вы ввели: 4321_
+checkPassword('qaz-xsw'); // Ого, надёжный пароль! Вы ввели: qaz-xsw
+checkPassword('_zxd'); // Ого, надёжный пароль! Вы ввели: _zxd
+checkPassword('_-a'); // Внимание! Ненадёжный пароль.. Вы ввели: _-a
+checkPassword('qaz'); // Внимание! Ненадёжный пароль.. Вы ввели: qaz
+checkPassword('_-3'); // Внимание! Ненадёжный пароль.. Вы ввели: _-3
