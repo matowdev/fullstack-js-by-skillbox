8e719846b6c26536656f870d299b655466c36af2
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-11-02 08:33:27 +0300

    [LES-4.8/compl] array-mixing
    Array mixing or the Fisher-Yates shuffle. Random numbers.
    
    Deserv(s) attention:
      - index/value overwrite logic.
    
    Core: B-3 / JS-BL

--- Core_Courses/3_JS_Basic_Level/4-arrays-cycles/4-8-array-mixing/index.js
+++ Core_Courses/3_JS_Basic_Level/4-arrays-cycles/4-8-array-mixing/index.js
@@ -20,7 +20,11 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let tempTemp;
 
 for (let i = 0; i <= arr.length - 1; i++) {
-  console.log(arr[i]);
+  let j = Math.floor(Math.abs(Math.random() * arr.length - 1));
+  // console.log(j); // 7, 8, 1, 7, 4, 7, 0, 0, 8, 7
+  tempTemp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tempTemp;
 }
 
-// добавить синтаксис в iDo..
+console.log(arr); // (10) [6, 3, 9, 1, 5, 4, 8, 10, 2, 7]
