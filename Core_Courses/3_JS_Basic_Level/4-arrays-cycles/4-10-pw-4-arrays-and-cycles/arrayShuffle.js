c482343f19938f4083d986e3d4f7c3d1c1a71178
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-11-16 08:12:44 +0300

    [PW-4.10/cont] Add 2 of 3 tasks
    Create array of ordered num's, shuffle it accord. "Fisher - Yates" prin.
    
    Core: B-3 / JS-BL

---
+++ Core_Courses/3_JS_Basic_Level/4-arrays-cycles/4-10-pw-4-arrays-and-cycles/arrayShuffle.js
@@ -0,0 +1,37 @@
+// Что нужно сделать
+// 1. Создайте с помощью цикла for массив упорядоченных чисел с количеством чисел, равным count.
+// 2. С помощью второго цикла перемешайте этот массив.
+// 3. Выведите получившийся результат на экран с помощью console.log.
+
+function getOrderedArr(count) {
+  let orderArr = [];
+
+  for (let i = 1; i <= count; i++) {
+    orderArr.push(i);
+  }
+
+  return orderArr;
+}
+
+function getShuffledArr(count) {
+  let arr = getOrderedArr(count);
+  let temp;
+
+  for (let i = 0; i < arr.length; i++) {
+    let j = Math.floor(Math.random() * arr.length);
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  return arr;
+}
+
+let shuffledArr = getShuffledArr(5);
+console.log(shuffledArr); // (5) [2, 5, 1, 3, 4]
+
+let shuffledArr2 = getShuffledArr(7);
+console.log(shuffledArr2); // (7) [5, 1, 6, 4, 2, 7, 3]
+
+let shuffledArr3 = getShuffledArr(3);
+console.log(shuffledArr3); // (3) [2, 1, 3]
