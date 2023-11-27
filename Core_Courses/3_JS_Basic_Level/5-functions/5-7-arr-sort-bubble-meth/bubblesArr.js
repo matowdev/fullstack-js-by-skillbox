d6d8660ff54877efeb81eb81046611a0d349b411
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-11-27 15:14:29 +0300

    [LES-5.7/st-compl] arr-sort-bubble-meth
    Sorting arr "bubble" meth., moving up larger num. Shuffling principle.
    
    Deserv(s) attention:
      - on the next iteration of the outer loop, everything will be compared from the beginning.
    
    Core: B-3 / JS-BL

---
+++ Core_Courses/3_JS_Basic_Level/5-functions/5-7-arr-sort-bubble-meth/bubblesArr.js
@@ -0,0 +1,46 @@
+// функция сортировки массива методом "пузырька"
+function getSortBubblesArr(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      console.log(arr[j]);
+      if (arr[j] > arr[j + 1]) {
+        // если поменять знак, на < сортировка начнёт работать на убывание
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+let arrNum = [3, 22, 34, 12, 55, 22, 12, 3];
+
+let sortArr = getSortBubblesArr(arrNum);
+console.log(sortArr); // (8) [3, 3, 12, 12, 22, 22, 34, 55]
+
+//
+// функция сортировки массива методом "пузырька" с проверкой на операцию обмена (хоть что то менялось местами..?)
+function getSortBubblesArr2(arr) {
+  let swapped = true;
+
+  for (let i = 0; i < arr.length - 1 && swapped; i++) {
+    swapped = false;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swapped = true;
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+let arrNum2 = [3, 22, 34, 12, 55, 22, 12, 3];
+
+let sortArr2 = getSortBubblesArr(arrNum2);
+console.log(sortArr2); // (8) [3, 3, 12, 12, 22, 22, 34, 55]
---
+++ Core_Courses/3_JS_Basic_Level/5-functions/5-7-arr-sort-bubble-meth/notes.md
@@ -0,0 +1,5 @@
+# On the topic:
+
+### Video-lesson:
+
+[5.7. Array sorting. The "bubble" method](https://go.skillbox.ru/profession/profession-fullstack-js/js/6144610a-20f4-4ec1-b448-d0cf2c95b357/longread)
