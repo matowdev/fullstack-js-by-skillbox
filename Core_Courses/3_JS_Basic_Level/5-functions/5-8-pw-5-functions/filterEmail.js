02d8980c3ccff0060e9975f6ca227ef6d782a207
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-11-29 09:09:11 +0300

    [PW-5.8/cont] Add 2 of 3 tasks
    Interacting with two arrays, checking for unwanted mailboxes.
    
    Worth noting:
      - that code clarity and ease of understanding are of the highest priority.
    
    Core: B-3 / JS-BL

---
+++ Core_Courses/3_JS_Basic_Level/5-functions/5-8-pw-5-functions/filterEmail.js
@@ -0,0 +1,90 @@
+// ? Что нужно сделать
+// 1. Напишите функцию filter(), которая создаёт массив email-адресов, не попавших в чёрный список. В качестве аргументов функция должна принимать два массива: массив строк с исходными email-адресами и массив строк с email-адресами в чёрном списке.
+// 2. Выведите результат выполнения функции в консоль в виде массива.
+
+// первый вариант
+
+// функция для проверки наличия нежелательного имейла (согласно двух списков/массивов)
+function filter(whiteList, blackList) {
+  let newList = [];
+
+  for (let email of whiteList) {
+    if (blackList.includes(email)) continue;
+    newList.push(email);
+  }
+
+  return newList;
+}
+
+let whiteList = [
+  'my-email@gmail.ru',
+  'jsfunc@mail.ru',
+  'annavkmail@vk.ru',
+  'fullname@skill.ru',
+  'goodday@day.ru',
+];
+
+let blackList = ['jsfunc@mail.ru', 'goodday@day.ru'];
+
+// вызов/вывод результатов
+let resultList = filter(whiteList, blackList);
+console.log(resultList); // (3) ['my-email@gmail.ru', 'annavkmail@vk.ru', 'fullname@skill.ru']
+
+// *второй вариант
+
+// функция для проверки наличия нежелательного имейла (согласно двух списков/массивов)
+function filter2(whiteList, blackList) {
+  let newList = [];
+
+  for (let email of whiteList) {
+    if (!blackList.includes(email)) {
+      newList.push(email);
+    }
+  }
+
+  return newList;
+}
+
+let whiteList2 = [
+  'my-email@gmail.ru',
+  'jsfunc@mail.ru',
+  'annavkmail@vk.ru',
+  'fullname@skill.ru',
+  'goodday@day.ru',
+];
+
+let blackList2 = ['jsfunc@mail.ru', 'goodday@day.ru'];
+
+// вызов/вывод результатов
+let resultList2 = filter(whiteList, blackList);
+console.log(resultList2); // (3) ['my-email@gmail.ru', 'annavkmail@vk.ru', 'fullname@skill.ru']
+
+// третий вариант
+
+// функция для проверки наличия нежелательного имейла (согласно двух списков/массивов)
+function filter3(whiteList, blackList) {
+  let newList = [];
+
+  for (let i = 0; i < whiteList.length; i++) {
+    let email = whiteList[i];
+    if (blackList.indexOf(email) === -1) {
+      newList.push(email);
+    }
+  }
+
+  return newList;
+}
+
+let whiteList3 = [
+  'my-email@gmail.ru',
+  'jsfunc@mail.ru',
+  'annavkmail@vk.ru',
+  'fullname@skill.ru',
+  'goodday@day.ru',
+];
+
+let blackList3 = ['jsfunc@mail.ru', 'goodday@day.ru'];
+
+// вызов/вывод результатов
+let resultList3 = filter(whiteList, blackList);
+console.log(resultList3); // (3) ['my-email@gmail.ru', 'annavkmail@vk.ru', 'fullname@skill.ru']
