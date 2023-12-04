5db543254fd07d0ecf3eceeb87c8b8553523684c
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-12-04 08:07:22 +0300

    [LES-6.1/st-compl] objects-intro
    Initial introduction to the JS object.
    
    Worth noting:
      - is not a "simple" data type.
    
    Core: B-3 / JS-BL

---
+++ Core_Courses/3_JS_Basic_Level/6-objects/6-1-objects-intro/index.js
@@ -0,0 +1,16 @@
+// пример объекта, синтаксиса
+let person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 50,
+  eyeColor: 'blue',
+};
+
+console.log(person);
+
+// Object i
+// age: 50
+// eyeColor: "blue"
+// firstName: "John"
+// lastName: "Doe"
+// [[Prototype]]: Object
---
+++ Core_Courses/3_JS_Basic_Level/6-objects/6-1-objects-intro/notes.md
@@ -0,0 +1,9 @@
+# On the topic:
+
+### Video-lesson:
+
+[6.1. Objects. Intro](https://go.skillbox.ru/profession/profession-fullstack-js/js/78413237-f893-45fa-bdb0-53461e710d67/videolesson)
+
+### A few words:
+
+Let's get started!
