f22ad1231055745d2cf36228386c22bd1862f9a5
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-01-09 16:17:26 +0300

    [LES-6.6/st-compl] type-system-typeof
    Definition/analysis of data types. Interaction with typeof operator.
    
    Deserv(s) attention:
      - data type of "null" and "function".
    
    core: B-3 / JS-BL

---
+++ core-courses/3-js-basic-level/6-objects/6-6-type-system-typeof/notes.md
@@ -0,0 +1,5 @@
+# On the topic:
+
+### Video-lesson:
+
+[6.6. Type system. Operator typeof.](https://go.skillbox.ru/profession/profession-fullstack-js/js/7aab2d29-0788-426a-95e3-9edab35d7712/videolesson)
---
+++ core-courses/3-js-basic-level/6-objects/6-6-type-system-typeof/typeOf.js
@@ -0,0 +1,39 @@
+// *примитивные типы данных (простые, нулевые)
+console.log(typeof 'abc'); // string
+console.log(typeof 1); // number
+// console.log(typeof NaN); // number
+console.log(typeof false); // boolean
+console.log(typeof 123n); // bigint
+console.log(typeof Symbol()); // symbol
+
+console.log(typeof null); // object, хотя напрашивается/очевидно null
+console.log(typeof undefined); // undefined
+
+// *НЕ примитивные типы данных
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(typeof function () {}); // function, хотя в общей системе JS типов нет такого понимания, как function
+
+// *пример использования typeof
+function getStringValue(unknown) {
+  if (typeof unknown === 'string') {
+    return unknown;
+  }
+  if (typeof unknown === 'function') {
+    return unknown();
+  }
+  if (typeof unknown === 'object') {
+    return unknown.toString();
+  }
+  return '';
+}
+
+console.log(getStringValue('Просто строка')); // Просто строка
+console.log(
+  getStringValue(function () {
+    return new Date().getFullYear();
+  })
+); // 2024
+console.log(getStringValue([1, 2, 3])); // 1,2,3
+console.log(getStringValue({})); // [object Object]
+console.log(getStringValue(true)); // ' '
