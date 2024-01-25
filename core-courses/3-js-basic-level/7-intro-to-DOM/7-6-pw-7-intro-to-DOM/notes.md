e3741056d83e70ddff7a8dbd5623c8a7234db47d
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-01-25 13:32:35 +0300

    [PW-7.6/start] Add 1 of 4 tasks
    Working with DOM, "student card" gen., strictly from JS. "Easy" styling.
    
    Worth noting:
      - the interestingness of this task.
    
    core: B-3 / JS-BL

---
+++ core-courses/3-js-basic-level/7-intro-to-DOM/7-6-pw-7-intro-to-DOM/notes.md
@@ -0,0 +1,9 @@
+# On the topic:
+
+### Terms of performance:
+
+[7.6. PW #7. Introduction to DOM.](https://go.skillbox.ru/profession/profession-fullstack-js/js/e5a833fb-ac91-46c2-8aa8-c251e1ff15e7/homework)
+
+### A few words:
+
+Four tasks. Let's do it!
---
+++ core-courses/3-js-basic-level/7-intro-to-DOM/7-6-pw-7-intro-to-DOM/task-1/index.js
@@ -0,0 +1,29 @@
+// ? Что нужно сделать
+// 1. Создайте базовый HTML-код и подключите к нему JavaScript-файл.
+// 2. Создайте функцию с названием createStudentCard() в js-файле, принимающую два параметра: name и age. Функция должна создавать карточку студента внутри элемента body HTML-страницы.
+// Карточка студента представляет собой DOM-элемент, а именно тег div, внутри которого находится заголовок h2 с именем студента из параметра name и span под заголовком с возрастом студента (age).
+// При желании можно украсить элементы CSS-стилизацией.
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    document.body.classList.add('page');
+
+    function createStudentCard(name, age) {
+      let divCard = document.createElement('div');
+      let cardHeader = document.createElement('h2');
+      let cardSubHeader = document.createElement('span');
+
+      divCard.classList.add('card', 'page__student-card');
+      cardHeader.classList.add('card__header');
+      cardSubHeader.classList.add('card__sub-header');
+
+      cardHeader.textContent = name;
+      cardSubHeader.textContent = `Возраст: ${age} лет`;
+
+      divCard.append(cardHeader, cardSubHeader);
+      document.body.append(divCard);
+    }
+
+    createStudentCard('Игорь', 17);
+  });
+})();
