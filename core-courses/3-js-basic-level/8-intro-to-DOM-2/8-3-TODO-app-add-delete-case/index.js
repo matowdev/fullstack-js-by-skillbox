cf94d884ca704466808ac9f98066cf7654913fe0
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-02-02 16:32:34 +0300

    [LES-8.3/st-compl] TODO-app-add-delete-case
    Adding cases to the list, working with "Done/Delete" btn's.
    
    Worth noting:
      - admiration for the process.
    
    core: B-3 / JS-BL

---
+++ core-courses/3-js-basic-level/8-intro-to-DOM-2/8-3-TODO-app-add-delete-case/index.js
@@ -0,0 +1,116 @@
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    // создание/возвращением заголовка, для списка дел
+    function createTodoFormListTitle(title) {
+      let formListTitle = document.createElement('h2');
+
+      formListTitle.classList.add('todo__header');
+      formListTitle.textContent = title;
+
+      return formListTitle;
+    }
+
+    // создание/возвращение формы/поля, для ввода дел
+    function createTodoFormElement() {
+      let form = document.createElement('form');
+      let formInput = document.createElement('input');
+      let formBtnWrap = document.createElement('div');
+      let formBtn = document.createElement('button');
+
+      form.classList.add('todo__form', 'input-group', 'mb-3');
+      formInput.classList.add('todo__form-input', 'form-control');
+      formBtnWrap.classList.add('todo__form-btn-wrap', 'input-group-append');
+      formBtn.classList.add('todo__form-btn', 'btn', 'btn-primary');
+
+      formInput.placeholder = 'Введите название дела..';
+      formBtn.textContent = 'Добавить!?';
+
+      formBtnWrap.append(formBtn);
+      form.append(formInput);
+      form.append(formBtnWrap);
+
+      return { form, formInput, formBtn };
+    }
+
+    // создание/возвращение списка дел
+    function createTodoFormList() {
+      let formList = document.createElement('ul');
+      formList.classList.add('todo__list', 'list-group');
+
+      return formList;
+    }
+
+    // создание/возвращение дела (внутренних кнопок)
+    function createTodoFormListItem(name) {
+      let formListItem = document.createElement('li');
+      let btnItemGroup = document.createElement('div');
+      let doneBtn = document.createElement('button');
+      let deleteBtn = document.createElement('button');
+
+      formListItem.classList.add(
+        'todo__item',
+        'list-group-item',
+        'd-flex',
+        'justify-content-between',
+        'align-items-center'
+      );
+      btnItemGroup.classList.add(
+        'todo__item-btn-group',
+        'btn-group',
+        'btn-group-sm'
+      );
+      doneBtn.classList.add('todo__item-btn', 'btn', 'btn-success');
+      deleteBtn.classList.add('todo__item-btn', 'btn', 'btn-danger');
+
+      formListItem.textContent = name;
+      doneBtn.textContent = 'Готово';
+      deleteBtn.textContent = 'Удалить';
+
+      btnItemGroup.append(doneBtn);
+      btnItemGroup.append(deleteBtn);
+      formListItem.append(btnItemGroup);
+
+      return { formListItem, doneBtn, deleteBtn };
+    }
+
+    // увязка составляющих
+    let todo = document.getElementById('todo-app');
+    let todoListTitle = createTodoFormListTitle('Список дел:');
+    let todoItem = createTodoFormElement();
+    let todoList = createTodoFormList();
+
+    todo.append(todoListTitle);
+    todo.append(todoItem.form); // из тройного возврата (в/из функции createTodoFormElement()) выбирается form, по сути "весь" составной элемент
+    todo.append(todoList);
+
+    todoItem.form.addEventListener('submit', function (e) {
+      e.preventDefault(); // исключение default перезагрузки страницы, при добавлении дела (как-бы/при отправке формы)
+
+      if (!todoItem.formInput.value) {
+        return;
+      }
+
+      // todoList.append(
+      //   createTodoFormListItem(todoItem.formInput.value).formListItem
+      // ); // логика добавления введённого дела из поля/формы для ввода, далее в список
+
+      let todoSelectedItem = createTodoFormListItem(todoItem.formInput.value);
+
+      todoSelectedItem.doneBtn.addEventListener('click', function () {
+        todoSelectedItem.formListItem.classList.toggle(
+          'list-group-item-success'
+        );
+      });
+
+      todoSelectedItem.deleteBtn.addEventListener('click', function () {
+        if (confirm('Вы уверены?')) {
+          todoSelectedItem.formListItem.remove();
+        }
+      });
+
+      todoList.append(todoSelectedItem.formListItem);
+
+      todoItem.formInput.value = ''; // очищение поля для ввода (после добавления дела)
+    });
+  });
+})();
---
+++ core-courses/3-js-basic-level/8-intro-to-DOM-2/8-3-TODO-app-add-delete-case/notes.md
@@ -0,0 +1,5 @@
+# On the topic:
+
+### Video-lesson:
+
+[8.3. TODO app. Adding, deleting cases.](https://go.skillbox.ru/profession/profession-fullstack-js/js/8961d1aa-d0c8-47a0-8992-2d8d4e15593e/videolesson)
