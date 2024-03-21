9a0f4c1e54afed8d88050ab6112a46d82f9a8175
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-03-21 08:14:29 +0300

    [PW-9.7/cont] Add infinitePlayback.js file
    Realiz. infinite playback, accord. tracks from arr. Not via audio/loop.
    
    Deserv(s) attention:
      - postfix increment++ operation, playback will not start from 0.
    
    core: B-3 / JS-BL

---
+++ core-courses/3-js-basic-level/9-const-scopes-closures/9-7-pw-9-const-scopes-closures/js/infinitePlayback.js
@@ -0,0 +1,23 @@
+(() => {
+  let tracksArr = [
+    'tracks/brain.mp3',
+    'tracks/close.mp3',
+    'tracks/golden.mp3',
+    'tracks/sigh.mp3',
+    'tracks/sky.mp3',
+  ];
+  let currentTrack = -1;
+  const audioPlayer = document.querySelector('.footer__audio');
+
+  audioPlayer.onended = function () {
+    currentTrack++;
+
+    if (currentTrack >= tracksArr.length) {
+      currentTrack = 0;
+    }
+
+    audioPlayer.src = tracksArr[currentTrack];
+    audioPlayer.load();
+    audioPlayer.play();
+  };
+})();
