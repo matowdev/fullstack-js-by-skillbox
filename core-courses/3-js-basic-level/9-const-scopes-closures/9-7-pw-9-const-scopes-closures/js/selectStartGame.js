04bd295753afd1a93b4f9a23f2c4312ff7c9ff28
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-04-16 09:15:21 +0300

    [PW-9.7/cont] Fix "flipping/jerking" card(s)
    Fix card flip, when hov (after click out/field). Fix "jerking" cards.
    
    Deserv(s) attention:
      - adding additional exceptions, via :not in the any-hover directive.
      - replacing "ease-in-out" with "ease" (unexpectedly different/very behavior).
    
    core: B-3 / JS-BL

--- core-courses/3-js-basic-level/9-const-scopes-closures/9-7-pw-9-const-scopes-closures/js/selectStartGame.js
+++ core-courses/3-js-basic-level/9-const-scopes-closures/9-7-pw-9-const-scopes-closures/js/selectStartGame.js
@@ -152,7 +152,7 @@
     const newArr = [...pairedArr];
 
     for (let i = newArr.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
+      let j = Math.floor(Math.random() * newArr.length);
       [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
     }
 
