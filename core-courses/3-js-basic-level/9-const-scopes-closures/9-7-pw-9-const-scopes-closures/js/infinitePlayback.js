a7266a6cbebd697d0d48c94cd58fb3c36938ccc2
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-04-23 07:35:31 +0300

    [PW-9.7/cont] Add some changes: width/height, DOM
    Add fixed sizes for img by atr. Rem DOM loading logic from all js files.
    
    Worth noting:
      - experiments with the organization of image preloading.
    
    core: B-3 / JS-BL

--- core-courses/3-js-basic-level/9-const-scopes-closures/9-7-pw-9-const-scopes-closures/js/infinitePlayback.js
+++ core-courses/3-js-basic-level/9-const-scopes-closures/9-7-pw-9-const-scopes-closures/js/infinitePlayback.js
@@ -1,25 +1,23 @@
 (() => {
-  document.addEventListener('DOMContentLoaded', () => {
-    let tracksArr = [
-      'tracks/brain.mp3',
-      'tracks/close.mp3',
-      'tracks/golden.mp3',
-      'tracks/sigh.mp3',
-      'tracks/sky.mp3',
-    ];
-    let currentTrack = -1;
-    const audioPlayer = document.querySelector('.footer__audio-player');
+  let tracksArr = [
+    'tracks/brain.mp3',
+    'tracks/close.mp3',
+    'tracks/golden.mp3',
+    'tracks/sigh.mp3',
+    'tracks/sky.mp3',
+  ];
+  let currentTrack = -1;
+  const audioPlayer = document.querySelector('.footer__audio-player');
 
-    audioPlayer.onended = function () {
-      currentTrack++;
+  audioPlayer.onended = function () {
+    currentTrack++;
 
-      if (currentTrack >= tracksArr.length) {
-        currentTrack = 0;
-      }
+    if (currentTrack >= tracksArr.length) {
+      currentTrack = 0;
+    }
 
-      audioPlayer.src = tracksArr[currentTrack];
-      audioPlayer.load();
-      audioPlayer.play();
-    };
-  });
+    audioPlayer.src = tracksArr[currentTrack];
+    audioPlayer.load();
+    audioPlayer.play();
+  };
 })();
