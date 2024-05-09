cdb23a2c5ff032bdead28df16c3e40067a9d3d68
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-05-09 07:05:54 +0300

    [LES-10.5/compl] practice-calls-chain
    Getting all links from the page (list/arr of domains).
    
    Worth noting:
      - this realization, the calls-chain manual.
    
    core: B-3 / JS-BL

--- core-courses/3-js-basic-level/10-type-conversion-non-strict/10-5-practice-calls-chain/getPageLinks.js
+++ core-courses/3-js-basic-level/10-type-conversion-non-strict/10-5-practice-calls-chain/getPageLinks.js
@@ -1,1 +1,35 @@
 // ** получение всех ссылок со страницы (список доменов)
+function getPageLinkDomains() {
+  return console.log(
+    Array.from(document.getElementsByTagName('a'))
+      .map((link) =>
+        link.href
+          .replace('http://', '')
+          .replace('https://', '')
+          .replace('www.', '')
+          .split('/')
+          .shift()
+      )
+      .filter((link) => link.trim().length > 0)
+      .reduce((uniqueDomains, domain) => {
+        if (uniqueDomains.includes(domain)) return uniqueDomains;
+        return [...uniqueDomains, domain];
+      }, [])
+  );
+}
+
+/*
+результат запуска функции/в console, на странице данного видео-урока
+
+['go.skillbox.ru', 'skillbox.ru', 'developer.mozilla.org', 'habr.com', 'zakirov.net', 'learn.javascript.ru', 'dposkillbox.ru']
+
+0: "go.skillbox.ru"
+1: "skillbox.ru"
+2: "developer.mozilla.org"
+3: "habr.com"
+4: "zakirov.net"
+5: "learn.javascript.ru"
+6: "dposkillbox.ru"
+
+length: 7
+*/
