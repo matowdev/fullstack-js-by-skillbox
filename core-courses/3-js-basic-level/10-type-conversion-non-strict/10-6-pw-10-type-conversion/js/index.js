cc86a2d4fa7985243186960834b35595210bed48
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-05-17 15:41:04 +0300

    [PW-10.6/cont] Add "form" structure
    Organizing form/inputs for entering student data (accord init arr).
    
    Worth noting:
      - Bootstrap library features.
    
    core: B-3 / JS-BL

--- core-courses/3-js-basic-level/10-type-conversion-non-strict/10-6-pw-10-type-conversion/js/index.js
+++ core-courses/3-js-basic-level/10-type-conversion-non-strict/10-6-pw-10-type-conversion/js/index.js
@@ -50,7 +50,157 @@
   const dboardFilter = document.getElementById('dboard-filter');
   const dboardOutput = document.getElementById('dboard-output');
 
-  // организация таблицы (стилизация)
+  // организация формы (ввод данных о студентах)
+  const formInputData = document.createElement('form');
+  const formInSurnameWrap = document.createElement('div');
+  const formInSurnameInput = document.createElement('input');
+  const formInSurnameLabel = document.createElement('label');
+  const formInNameWrap = document.createElement('div');
+  const formInNameInput = document.createElement('input');
+  const formInNameLabel = document.createElement('label');
+  const formInPatronymicWrap = document.createElement('div');
+  const formInPatronymicInput = document.createElement('input');
+  const formInPatronymicLabel = document.createElement('label');
+  const formInBirthDateWrap = document.createElement('div');
+  const formInBirthDateInput = document.createElement('input');
+  const formInBirthDateLabel = document.createElement('label');
+  const formInStartYearWrap = document.createElement('div');
+  const formInStartYearInput = document.createElement('input');
+  const formInStartYearLabel = document.createElement('label');
+  const formInFacultyWrap = document.createElement('div');
+  const formInFacultyInput = document.createElement('input');
+  const formInFacultyLabel = document.createElement('label');
+  const formInBtnWrap = document.createElement('div');
+  const formInBtnAdd = document.createElement('button');
+  const formInBtnClear = document.createElement('button');
+
+  formInputData.classList.add('dboard__input-form', 'mb-3');
+  formInSurnameWrap.classList.add(
+    'dboard__input-surname-wrap',
+    'form-floating',
+    'mb-3'
+  );
+  formInSurnameInput.classList.add(
+    'dboard__input-surname-input',
+    'form-control'
+  );
+  formInSurnameLabel.classList.add('dboard__input-surname-label');
+  formInNameWrap.classList.add(
+    'dboard__input-name-wrap',
+    'form-floating',
+    'mb-3'
+  );
+  formInNameInput.classList.add('dboard__input-name-input', 'form-control');
+  formInNameLabel.classList.add('dboard__input-name-label');
+  formInPatronymicWrap.classList.add(
+    'dboard__input-patron-wrap',
+    'form-floating',
+    'mb-3'
+  );
+  formInPatronymicInput.classList.add(
+    'dboard__input-patron-input',
+    'form-control'
+  );
+  formInPatronymicLabel.classList.add('dboard__input-patron-label');
+  formInBirthDateWrap.classList.add(
+    'dboard__input-birthday-wrap',
+    'form-floating',
+    'mb-3'
+  );
+  formInBirthDateInput.classList.add(
+    'dboard__input-birthday-input',
+    'form-control'
+  );
+  formInBirthDateLabel.classList.add('dboard__input-birthday-label');
+  formInStartYearWrap.classList.add(
+    'dboard__input-start-wrap',
+    'form-floating',
+    'mb-3'
+  );
+  formInStartYearInput.classList.add(
+    'dboard__input-start-input',
+    'form-control'
+  );
+  formInStartYearLabel.classList.add('dboard__input-start-label');
+  formInFacultyWrap.classList.add(
+    'dboard__input-faculty-wrap',
+    'form-floating',
+    'mb-3'
+  );
+  formInFacultyInput.classList.add(
+    'dboard__input-faculty-input',
+    'form-control'
+  );
+  formInFacultyLabel.classList.add('dboard__input-faculty-label');
+  formInBtnWrap.classList.add(
+    'dboard__input-btn-wrap',
+    'd-inline-flex',
+    'gap-2'
+  );
+  formInBtnAdd.classList.add('dboard__input-btn-add', 'btn', 'btn-primary');
+  formInBtnClear.classList.add(
+    'dboard__input-btn-clear',
+    'btn',
+    'btn-secondary'
+  );
+
+  formInputData.setAttribute('action', '#');
+  formInSurnameInput.setAttribute('id', 'floatingInputSurname');
+  formInSurnameInput.setAttribute('type', 'text');
+  formInSurnameInput.setAttribute('placeholder', 'Фамилия');
+  formInSurnameLabel.setAttribute('for', 'floatingInputSurname');
+  formInNameInput.setAttribute('id', 'floatingInputName');
+  formInNameInput.setAttribute('type', 'text');
+  formInNameInput.setAttribute('placeholder', 'Имя');
+  formInNameLabel.setAttribute('for', 'floatingInputName');
+  formInPatronymicInput.setAttribute('id', 'floatingInputPatronymic');
+  formInPatronymicInput.setAttribute('type', 'text');
+  formInPatronymicInput.setAttribute('placeholder', 'Отчество');
+  formInPatronymicLabel.setAttribute('for', 'floatingInputPatronymic');
+  formInBirthDateInput.setAttribute('id', 'floatingInputBirthday');
+  formInBirthDateInput.setAttribute('type', 'date'); // ?
+  formInBirthDateInput.setAttribute('placeholder', 'Дата рождения');
+  formInBirthDateLabel.setAttribute('for', 'floatingInputBirthday');
+  formInStartYearInput.setAttribute('id', 'floatingInputStartYear');
+  formInStartYearInput.setAttribute('type', 'number');
+  formInStartYearInput.setAttribute('placeholder', 'Год начала обучения');
+  formInStartYearInput.setAttribute('value', '2015'); // ?
+  formInStartYearLabel.setAttribute('for', 'floatingInputStartYear');
+  formInFacultyInput.setAttribute('id', 'floatingInputFaculty');
+  formInFacultyInput.setAttribute('type', 'text');
+  formInFacultyInput.setAttribute('placeholder', 'Факультет');
+  formInFacultyLabel.setAttribute('for', 'floatingInputFaculty');
+  formInBtnAdd.setAttribute('type', 'submit');
+  formInBtnClear.setAttribute('type', 'button'); // ?
+
+  formInSurnameLabel.textContent = 'Фамилия';
+  formInNameLabel.textContent = 'Имя';
+  formInPatronymicLabel.textContent = 'Отчество';
+  formInBirthDateLabel.textContent = 'Дата рождения';
+  formInStartYearLabel.textContent = 'Год начала обучения';
+  formInFacultyLabel.textContent = 'Факультет';
+  formInBtnAdd.textContent = 'Добавить студента';
+  formInBtnClear.textContent = 'Очистить поля ввода';
+
+  formInSurnameWrap.append(formInSurnameInput, formInSurnameLabel);
+  formInNameWrap.append(formInNameInput, formInNameLabel);
+  formInPatronymicWrap.append(formInPatronymicInput, formInPatronymicLabel);
+  formInBirthDateWrap.append(formInBirthDateInput, formInBirthDateLabel);
+  formInStartYearWrap.append(formInStartYearInput, formInStartYearLabel);
+  formInFacultyWrap.append(formInFacultyInput, formInFacultyLabel);
+  formInBtnWrap.append(formInBtnAdd, formInBtnClear);
+  formInputData.append(
+    formInSurnameWrap,
+    formInNameWrap,
+    formInPatronymicWrap,
+    formInBirthDateWrap,
+    formInStartYearWrap,
+    formInFacultyWrap,
+    formInBtnWrap
+  );
+  dboardInput.append(formInputData);
+
+  // организация таблицы (вывод информации о студентах)
   const table = document.createElement('table');
   const tableHead = document.createElement('thead');
   const tableBody = document.createElement('tbody');
@@ -67,6 +217,7 @@
     'table-bordered',
     'table-hover'
   );
+  tableHead.classList.add('dboard__table-head');
   tableBody.classList.add('dboard__table-body', 'table-group-divider');
 
   tableHeaderThTag.textContent = '#';
